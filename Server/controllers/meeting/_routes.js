93aa9315b5b648d705849601f9f74e072c0f4ad3
Ramin-Bateni <ram_20designer@yahoo.com>
2025-08-02 00:12:25 +0330

    refactor(server): update meeting routes to new patterns

--- Server/controllers/meeting/_routes.js
+++ Server/controllers/meeting/_routes.js
@@ -1,5 +1,29 @@
-const express = require('express');
+const express = require("express");
+const meeting = require("./meeting");
+const auth = require("../../middelwares/auth.js");
+const {
+  validateCreate,
+  validateUpdate,
+} = require("../../validators/meetingValidator.js");
 
 const router = express.Router();
 
-module.exports = router
+// List all meetings
+router.get("/", auth, meeting.index);
+
+// Add a new meeting
+router.post("/add", auth, validateCreate, meeting.add);
+
+// View a meeting by ID
+router.get("/view/:id", auth, meeting.view);
+
+// Edit a meeting by ID
+router.put("/edit/:id", auth, validateUpdate, meeting.update);
+
+// Delete a meeting by ID
+router.delete("/delete/:id", auth, meeting.deleteData);
+
+// Bulk delete meetings
+router.post("/deleteMany", auth, meeting.deleteMany);
+
+module.exports = router;
